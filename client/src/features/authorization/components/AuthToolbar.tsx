59505c2606954a91f90f65c6d7f3a35c5485b190
Wronqa <kuba.wrona@onet.pl>
2023-04-02 17:28:42 +0200

    Created component

---
+++ client/src/features/authorization/components/AuthToolbar.tsx
@@ -0,0 +1,36 @@
+import { Box, Typography, Button } from '@mui/material';
+import React from 'react';
+import CustomLink from '../../../components/common/CustomLink';
+import Logo from '../../../components/common/Logo';
+import Navbar from '../../../components/common/Navbar';
+
+const AuthToolbar = () => {
+	return (
+		<Navbar>
+			<Box sx={{ display: 'flex', alignItems: 'center' }}>
+				<Logo />
+			</Box>
+			<Box sx={{ display: { xs: 'none', md: 'flex', alignItems: 'center' } }}>
+				<Typography
+					sx={{ mr: 2, cursor: 'pointer' }}
+					variant="button"
+					color="inherit"
+					component="div"
+					fontSize={11}
+				>
+					<CustomLink text="Zarejestruj się!" path="../signup" />
+				</Typography>
+				<Button
+					variant="contained"
+					color="success"
+					size="small"
+					sx={{ fontSize: 11 }}
+				>
+					<CustomLink text="Zaloguj się" path="../signin" />
+				</Button>
+			</Box>
+		</Navbar>
+	);
+};
+
+export default AuthToolbar;
