fb36fcabe1ec55141ac166e6e3f696ec96ae325d
Wronqa <kuba.wrona@onet.pl>
2023-04-02 17:25:23 +0200

    Divided navbar into two separate components

--- client/src/views/auth/SignUp.tsx
+++ client/src/views/auth/SignUp.tsx
@@ -5,34 +5,12 @@ import Navbar from '../../components/common/Navbar';
 import Form from '../../components/common/Form';
 import SignInForm from '../../features/authorization/components/SignInForm';
 import SignUpForm from '../../features/authorization/components/SignUpForm';
+import AuthToolbar from '../../features/authorization/components/AuthToolbar';
 
 const SignUp = () => {
 	return (
 		<div>
-			<Navbar>
-				<Box sx={{ display: 'flex', alignItems: 'center' }}>
-					<Logo />
-				</Box>
-				<Box sx={{ display: { xs: 'none', md: 'flex', alignItems: 'center' } }}>
-					<Typography
-						sx={{ mr: 2 }}
-						variant="button"
-						color="inherit"
-						component="div"
-						fontSize={11}
-					>
-						Zarejestruj się
-					</Typography>
-					<Button
-						variant="contained"
-						color="success"
-						size="small"
-						sx={{ fontSize: 11 }}
-					>
-						Zaloguj sie
-					</Button>
-				</Box>
-			</Navbar>
+			<AuthToolbar />
 			<Container component="main" maxWidth="xs">
 				<SignUpForm />
 			</Container>
