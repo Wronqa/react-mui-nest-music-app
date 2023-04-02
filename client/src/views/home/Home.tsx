fde1d32e2e6db990330234fe88d0f8a44b93b5ab
Wronqa <kuba.wrona@onet.pl>
2023-04-02 18:31:32 +0200

    Only for tests

--- client/src/views/home/Home.tsx
+++ client/src/views/home/Home.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import MainToolbar from '../../features/songs/components/MainToolbar';
+import CustomCard from '../../components/card/CustomCard';
 
 const Home = () => {
 	return (
 		<div>
 			<MainToolbar />
+			<CustomCard />
 		</div>
 	);
 };
