1981e160e443c91092256ffd07923f7700ce8373
Wronqa <kuba.wrona@onet.pl>
2023-04-02 15:52:53 +0200

    Edited messagees source

--- client/src/features/authorization/schemas/signInSchema.ts
+++ client/src/features/authorization/schemas/signInSchema.ts
@@ -1,11 +1,14 @@
 import { object, string, number, date, InferType } from 'yup';
+import { authMessages } from '../../../messages/localizedMessages';
 
 export let signInSchema = object({
-	email: string().email().required(),
+	email: string()
+		.email(authMessages.email_invalid)
+		.required(authMessages.required),
 	password: string()
-		.required()
+		.required(authMessages.required)
 		.matches(
 			/^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?])[A-Za-z\d!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]{8,}$/,
-			'Hasło musi zawierać minimum 8 znaków, przynajmniej jedną cyfre, znak specjalny oraz duża literę'
+			authMessages.password_strong
 		),
 });
