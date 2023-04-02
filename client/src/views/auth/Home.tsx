c7205804dda476c015ce45e07cd4eb4c2e41b515
Wronqa <kuba.wrona@onet.pl>
2023-04-02 17:52:53 +0200

    Created Home view

---
+++ client/src/views/auth/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MainToolbar from '../../features/songs/components/MainToolbar';
+
+const Home = () => {
+	return (
+		<div>
+			<MainToolbar />
+		</div>
+	);
+};
+
+export default Home;
