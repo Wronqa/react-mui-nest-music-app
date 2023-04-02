66008137a6634f134c6c2a4103f2ca80203f3400
Wronqa <kuba.wrona@onet.pl>
2023-04-02 19:54:41 +0200

    Reorganized structure

--- client/src/views/auth/SignUp.tsx
+++ client/src/views/auth/SignUp.tsx
@@ -5,12 +5,14 @@ import Navbar from '../../components/common/Navbar';
 import Form from '../../components/common/Form';
 import SignInForm from '../../features/authorization/components/SignInForm';
 import SignUpForm from '../../features/authorization/components/SignUpForm';
-import AuthToolbar from '../../features/authorization/components/AuthToolbar';
+import AuthToolbar from '../../features/navigation/AuthToolbar';
 
 const SignUp = () => {
 	return (
 		<div>
-			<AuthToolbar />
+			<Navbar>
+				<AuthToolbar />
+			</Navbar>
 			<Container component="main" maxWidth="xs">
 				<SignUpForm />
 			</Container>
