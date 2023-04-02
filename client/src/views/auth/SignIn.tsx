ab16e6fb078427994f724591ac9f42f00cacbb08
Wronqa <kuba.wrona@onet.pl>
2023-04-02 17:26:01 +0200

    Divided Navbar into two separate coponents

--- client/src/views/auth/SignIn.tsx
+++ client/src/views/auth/SignIn.tsx
@@ -4,34 +4,14 @@ import Logo from '../../components/common/Logo';
 import Navbar from '../../components/common/Navbar';
 import Form from '../../components/common/Form';
 import SignInForm from '../../features/authorization/components/SignInForm';
+import { Link } from 'react-router-dom';
+import CustomLink from '../../components/common/CustomLink';
+import AuthToolbar from '../../features/authorization/components/AuthToolbar';
 
 const SignIn = () => {
 	return (
 		<div>
-			<Navbar>
-				<Box sx={{ display: 'flex', alignItems: 'center' }}>
-					<Logo />
-				</Box>
-				<Box sx={{ display: { xs: 'none', md: 'flex', alignItems: 'center' } }}>
-					<Typography
-						sx={{ mr: 2 }}
-						variant="button"
-						color="inherit"
-						component="div"
-						fontSize={11}
-					>
-						Zarejestruj się
-					</Typography>
-					<Button
-						variant="contained"
-						color="success"
-						size="small"
-						sx={{ fontSize: 11 }}
-					>
-						Zaloguj sie
-					</Button>
-				</Box>
-			</Navbar>
+			<AuthToolbar />
 			<Container component="main" maxWidth="xs">
 				<SignInForm />
 			</Container>
