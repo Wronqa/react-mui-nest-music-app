2b74374e6bde64914110703252f7f1f4d18b996e
Wronqa <kuba.wrona@onet.pl>
2023-04-02 15:52:32 +0200

    Created file with localized messages for validation

---
+++ client/src/messages/localizedMessages.ts
@@ -0,0 +1,8 @@
+export const authMessages = {
+	required: 'Wypełnij wymagane pola',
+	email_invalid: 'Nieprawidłowy adres email',
+	password_strong:
+		'Hasło musi zawierać minimum 8 znaków, przynajmniej jedną cyfre, znak specjalny oraz duża literę',
+	username_invalid: 'Nazwa użytkownika musi mieć minimum 4 znaki',
+	password_invalid_confirmation: 'Hasła nie są takie same',
+};
