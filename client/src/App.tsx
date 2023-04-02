feb18ebbd60d4a3d2cc4d11efbb06b1c4e35e4b4
Wronqa <kuba.wrona@onet.pl>
2023-04-02 17:30:06 +0200

    Created new routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,10 +3,18 @@ import { createBrowserRouter, Form, RouterProvider } from 'react-router-dom';
 import SignIn from './views/auth/SignIn';
 import SignUp from './views/auth/SignUp';
 import { Container } from '@mui/material';
+import ForgotPassword from './views/auth/ForgotPassword';
 
 const router = createBrowserRouter([
-	{ path: '/signin', element: <SignIn /> },
-	{ path: '/signup', element: <SignUp /> },
+	{
+		path: '/auth',
+
+		children: [
+			{ path: 'signin', element: <SignIn /> },
+			{ path: 'signup', element: <SignUp /> },
+			{ path: 'forgot', element: <ForgotPassword /> },
+		],
+	},
 ]);
 
 function App() {
