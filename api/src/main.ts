056e1e3d68af2433826adbcddd15fb55e25d8aea
Wronqa <kuba.wrona@onet.pl>
2023-04-02 23:21:11 +0200

    Added cors

--- api/src/main.ts
+++ api/src/main.ts
@@ -7,11 +7,12 @@ import cookieSession = require('cookie-session');
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({ credentials: true, origin: true });
   app.use(
     cookieSession({
       keys: [process.env.COOKIE_KEY],
     }),
   );
-  await app.listen(3000);
+  await app.listen(5000);
 }
 bootstrap();
