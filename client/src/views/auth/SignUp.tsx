2316006c284409640e43c1dd32429920a8970c12
Wronqa <kuba.wrona@onet.pl>
2023-04-02 15:42:21 +0200

    Created sign up view

---
+++ client/src/views/auth/SignUp.tsx
@@ -0,0 +1,43 @@
+import { Box, Typography, Button, Container } from '@mui/material';
+import React from 'react';
+import Logo from '../../components/common/Logo';
+import Navbar from '../../components/common/Navbar';
+import Form from '../../components/common/Form';
+import SignInForm from '../../features/authorization/components/SignInForm';
+import SignUpForm from '../../features/authorization/components/SignUpForm';
+
+const SignUp = () => {
+	return (
+		<div>
+			<Navbar>
+				<Box sx={{ display: 'flex', alignItems: 'center' }}>
+					<Logo />
+				</Box>
+				<Box sx={{ display: { xs: 'none', md: 'flex', alignItems: 'center' } }}>
+					<Typography
+						sx={{ mr: 2 }}
+						variant="button"
+						color="inherit"
+						component="div"
+						fontSize={11}
+					>
+						Zarejestruj się
+					</Typography>
+					<Button
+						variant="contained"
+						color="success"
+						size="small"
+						sx={{ fontSize: 11 }}
+					>
+						Zaloguj sie
+					</Button>
+				</Box>
+			</Navbar>
+			<Container component="main" maxWidth="xs">
+				<SignUpForm />
+			</Container>
+		</div>
+	);
+};
+
+export default SignUp;
