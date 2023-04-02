b07511fbc864ca9795e37eb97df69c25fc2c4dd3
Wronqa <kuba.wrona@onet.pl>
2023-04-02 15:06:25 +0200

    Added react router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import { createBrowserRouter, Form, RouterProvider } from 'react-router-dom';
+import SignIn from './views/auth/SignIn';
+
+const router = createBrowserRouter([{ path: '/signin', element: <SignIn /> }]);
 
 function App() {
-	return <div className="App"></div>;
+	return <RouterProvider router={router} />;
 }
 
 export default App;
