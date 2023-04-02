8fa2443c525c43ee8487de1fc9c4c416b1e4ed6e
Wronqa <kuba.wrona@onet.pl>
2023-04-02 15:04:31 +0200

    Created navbar

--- client/src/components/common/Navbar.tsx
+++ client/src/components/common/Navbar.tsx
@@ -1,7 +1,42 @@
-import React from 'react';
+import { ThemeProvider } from '@emotion/react';
+import {
+	AppBar,
+	Box,
+	Button,
+	Toolbar,
+	Typography,
+	createTheme,
+} from '@mui/material';
+import Logo from './Logo';
+import { ReactNode } from 'react';
 
-const Navbar = () => {
-	return <div>Halo</div>;
+interface NavbarProps {
+	children: ReactNode;
+}
+const Navbar = ({ children }: NavbarProps) => {
+	const darkTheme = createTheme({
+		palette: {
+			primary: {
+				main: 'rgb(27, 18, 18)',
+			},
+		},
+	});
+	return (
+		<ThemeProvider theme={darkTheme}>
+			<AppBar position="static" color="primary" enableColorOnDark>
+				<Toolbar
+					variant="dense"
+					sx={{
+						display: 'flex',
+						alignItems: 'center',
+						justifyContent: 'space-between',
+					}}
+				>
+					{children}
+				</Toolbar>
+			</AppBar>
+		</ThemeProvider>
+	);
 };
 
 export default Navbar;
