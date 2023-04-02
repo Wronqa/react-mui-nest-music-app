30da7b57e8aa7d40c1140aab29a2581dcb54525f
Wronqa <kuba.wrona@onet.pl>
2023-04-02 23:19:54 +0200

    Changed url

--- client/src/tools/Api.ts
+++ client/src/tools/Api.ts
@@ -1,14 +1,19 @@
 import axios, { AxiosInstance } from 'axios';
+import { RegisterDataInterface } from '../self_types/types';
 
 export class Api {
 	private static axiosInstance: AxiosInstance;
 
 	public static async initAxios() {
 		Api.axiosInstance = axios.create({
-			baseURL: 'https://some-domain.com/api/',
+			baseURL: 'http://localhost:5000',
 			timeout: 1000,
 			withCredentials: true,
 		});
 	}
-	static async signup() {}
+	static async signup(data: RegisterDataInterface) {
+		return Api.axiosInstance.post('/auth/signup', data, {
+			headers: { 'Access-Control-Allow-Origin': '*' },
+		});
+	}
 }
