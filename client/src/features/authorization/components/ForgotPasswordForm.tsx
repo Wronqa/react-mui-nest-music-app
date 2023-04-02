0e9cf83882770e7b05730a8f4dc18453de22c498
Wronqa <kuba.wrona@onet.pl>
2023-04-02 17:28:29 +0200

    Created new form for forgot password opstion

---
+++ client/src/features/authorization/components/ForgotPasswordForm.tsx
@@ -0,0 +1,45 @@
+import { TextField, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import { forgotPasswordSchema } from '../schemas/forgotPasswordSchema';
+import { useValidate } from '../hooks/useValidate';
+import Form from '../../../components/common/Form';
+import { ForgotPasswordInterface } from '../../../self_types/types';
+
+const ForgotPasswordForm = () => {
+	const [email, setEmail] = useState<string>('');
+
+	const errors = useValidate(
+		{ email } as ForgotPasswordInterface,
+		forgotPasswordSchema
+	);
+
+	const onSend = async (e: React.MouseEvent<HTMLButtonElement>) => {
+		e.preventDefault();
+	};
+
+	return (
+		<Form
+			handleClick={(e) => onSend(e)}
+			text="Przypomnij hasło!"
+			options={{ signup: false, forgotPassword: false }}
+		>
+			<TextField
+				margin="normal"
+				required
+				fullWidth
+				id="email"
+				label="Adres email"
+				name="email"
+				autoComplete="email"
+				autoFocus
+				value={email}
+				onChange={(e) => setEmail(e.target.value)}
+			/>
+			<Typography sx={{ color: 'red', fontSize: '0.8rem' }}>
+				{errors && errors[0]}
+			</Typography>
+		</Form>
+	);
+};
+
+export default ForgotPasswordForm;
