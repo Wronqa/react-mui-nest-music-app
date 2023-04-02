9c888f64bb03892231464954204d9e62381ed883
Wronqa <kuba.wrona@onet.pl>
2023-04-02 19:56:19 +0200

    Created admin page

---
+++ client/src/views/admin/Manage.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import AdminToolbar from '../../features/navigation/AdminToolbar';
+import Navbar from '../../components/common/Navbar';
+
+const Manage = () => {
+	return (
+		<div>
+			<Navbar>
+				<AdminToolbar />
+			</Navbar>
+		</div>
+	);
+};
+
+export default Manage;
