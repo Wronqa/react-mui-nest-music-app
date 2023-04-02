9f991d53ccd905e44442a7d8fb8cc8f46ec6f70c
Wronqa <kuba.wrona@onet.pl>
2023-04-02 15:04:15 +0200

    Created logic for signin

---
+++ client/src/features/authorization/components/SignInForm.tsx
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from 'react';
+import Form from '../../../components/common/Form';
+import { TextField, Typography } from '@mui/material';
+import { useValidate } from '../hooks/useValidate';
+import { signInSchema } from '../schemas/signInSchema';
+import { LoginDataInterface } from '../../../self_types/types';
+
+const SignInForm = () => {
+	const [data, setData] = useState<LoginDataInterface>({
+		email: '',
+		password: '',
+	});
+
+	const errors = useValidate(data, signInSchema);
+
+	console.log(errors);
+	const onSend = async (e: any) => {
+		e.preventDefault();
+	};
+
+	// (async () => {
+	// 	const user = await userSchema.validate(data);
+	// 	console.log(user);
+	// })();
+
+	return (
+		<Form handleClick={(e) => onSend(e)}>
+			<TextField
+				margin="normal"
+				required
+				fullWidth
+				id="email"
+				label="Adres email"
+				name="email"
+				autoComplete="email"
+				autoFocus
+				value={data.email}
+				onChange={(e) => setData({ ...data, email: e.target.value })}
+			/>
+
+			<TextField
+				margin="normal"
+				required
+				fullWidth
+				name="password"
+				label="Hasło"
+				type="password"
+				id="password"
+				autoComplete="current-password"
+				value={data.password}
+				onChange={(e) => setData({ ...data, password: e.target.value })}
+			/>
+			<Typography sx={{ color: 'red', fontSize: '0.8rem' }}>
+				{errors && errors[0]}
+			</Typography>
+		</Form>
+	);
+};
+
+export default SignInForm;
