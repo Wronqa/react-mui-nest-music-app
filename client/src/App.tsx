090e30ff373761b02c57e458a10ffb6ff944fdd1
Wronqa <kuba.wrona@onet.pl>
2023-04-02 15:44:38 +0200

    Added new route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import { createBrowserRouter, Form, RouterProvider } from 'react-router-dom';
 import SignIn from './views/auth/SignIn';
+import SignUp from './views/auth/SignUp';
+import { Container } from '@mui/material';
 
-const router = createBrowserRouter([{ path: '/signin', element: <SignIn /> }]);
+const router = createBrowserRouter([
+	{ path: '/signin', element: <SignIn /> },
+	{ path: '/signup', element: <SignUp /> },
+]);
 
 function App() {
 	return <RouterProvider router={router} />;
