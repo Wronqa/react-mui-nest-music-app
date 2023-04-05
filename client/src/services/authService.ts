dcf8634b82a63165de160b3df38ebdc2b3cbda84
Wronqa <kuba.wrona@onet.pl>
2023-04-06 01:04:00 +0200

    Deleted files

--- client/src/services/authService.ts
+++
@@ -1,9 +0,0 @@
-import { LoginDataInterface, RegisterDataInterface } from '../self_types/types';
-import { Api } from '../tools/Api';
-
-export const signUpService = async (data: RegisterDataInterface) => {
-	return Api.signup(data);
-};
-export const signInService = async (data: LoginDataInterface) => {
-	return Api.signin(data);
-};
