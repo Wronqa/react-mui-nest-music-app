c1c9fe0554aff8d623c40b752450031849bc696d
Wronqa <kuba.wrona@onet.pl>
2023-04-07 00:19:42 +0200

    Created search song functionality

--- client/src/views/home/Home.tsx
+++ client/src/views/home/Home.tsx
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
 import HomeToolbar from '../../features/navigation/HomeToolbar';
 import CustomCard from '../../components/card/CustomCard';
 import SongsContainer from '../../features/songs/components/SongsContainer';
 import Navbar from '../../components/common/Navbar';
 import Player from '../../features/songs/components/Player';
+import { searchSongService } from '../../services/songService';
+import { useMutation } from 'react-query';
+import { SongInterface } from '../../shared/interfaces/song.interface';
+import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../context/auth.context';
 
+////Zrób duze okienko do wyszukiwania
 const Home = () => {
+	const [songs, setSongs] = useState<SongInterface[]>([]);
+	const mutation = useMutation({
+		mutationFn: searchSongService,
+		onSuccess(response, variables, context) {
+			setSongs([response.data]);
+		},
+	});
+
+	const searchSongs = (query: string) => {
+		mutation.mutate(query);
+	};
+
 	return (
 		<div>
 			<Navbar>
-				<HomeToolbar />
+				<HomeToolbar onSearch={searchSongs} />
 			</Navbar>
-			<SongsContainer title="Twoja biblioteka" />
+			<SongsContainer title="Twoja biblioteka" songs={songs} />
 			<Player />
 		</div>
 	);
