80e31ed34054b5255c712406c8281620ee7d1d58
Wronqa <kuba.wrona@onet.pl>
2023-04-07 00:22:17 +0200

    Added props and display passed data

--- client/src/components/card/CustomCard.tsx
+++ client/src/components/card/CustomCard.tsx
@@ -12,15 +12,19 @@ import { link } from 'fs';
 import CardHeader from './CardHeader';
 import CardControl from './CardControl';
 import CardImage from './CardImage';
+import { SongInterface } from '../../shared/interfaces/song.interface';
 
-const CustomCard = () => {
+interface CustomCardProps {
+	song: SongInterface;
+}
+const CustomCard = ({ song }: CustomCardProps) => {
 	return (
 		<Card sx={{ display: 'flex', mr: 2, mt: 2, backgroundColor: '#f3f2ed' }}>
 			<Box sx={{ display: 'flex', flexDirection: 'column' }}>
-				<CardHeader />
+				<CardHeader title={song.title} artist={song.artist} />
 				<CardControl />
 			</Box>
-			<CardImage />
+			<CardImage album={song.album} />
 		</Card>
 	);
 };
