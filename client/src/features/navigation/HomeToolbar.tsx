da830a6810e8594e4dc1e70987af1a007aec9096
Wronqa <kuba.wrona@onet.pl>
2023-04-07 00:21:36 +0200

    Added logic for search songs

--- client/src/features/navigation/HomeToolbar.tsx
+++ client/src/features/navigation/HomeToolbar.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useContext } from 'react';
 import Navbar from '../../components/common/Navbar';
 import CustomLink from '../../components/common/CustomLink';
 import {
@@ -12,11 +12,28 @@ import {
 } from '@mui/material';
 import Logo from '../../components/common/Logo';
 import SearchBox from '../../components/common/SearchBox';
+import { useMutation } from 'react-query';
+import { logoutService } from '../../services/authServices';
+import { ACTIONS } from '../../shared/interfaces/auth.interface';
+import { AuthContext } from '../../context/auth.context';
 
 interface HomeToolbarProps {
 	children?: ReactNode;
+	onSearch?: (query: string) => void;
 }
-const HomeToolbar = ({ children }: HomeToolbarProps) => {
+const HomeToolbar = ({ children, onSearch }: HomeToolbarProps) => {
+	const { state, dispatch } = useContext(AuthContext);
+	console.log(state);
+	const mutation = useMutation({
+		mutationFn: logoutService,
+		onSuccess(response, variables, context) {
+			dispatch({ type: ACTIONS.loadUser, payload: response.data });
+		},
+	});
+
+	const logoutHandler = () => {
+		mutation.mutate();
+	};
 	return (
 		<>
 			<Box sx={{ display: 'flex', alignItems: 'center' }}>
@@ -39,7 +56,7 @@ const HomeToolbar = ({ children }: HomeToolbarProps) => {
 				>
 					Ulubione
 				</Typography>
-				<SearchBox />
+				<SearchBox onSearch={onSearch} />
 			</Box>
 			<Box sx={{ display: { xs: 'none', md: 'flex', alignItems: 'center' } }}>
 				{children}
@@ -49,6 +66,7 @@ const HomeToolbar = ({ children }: HomeToolbarProps) => {
 					color="inherit"
 					component="div"
 					fontSize={11}
+					onClick={logoutHandler}
 				>
 					Wyloguj
 				</Typography>
