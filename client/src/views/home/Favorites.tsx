d17c2955662ec078a266ee826991a3b1c934f3b5
Wronqa <kuba.wrona@onet.pl>
2023-04-07 23:19:44 +0200

    Temporary comment

--- client/src/views/home/Favorites.tsx
+++ client/src/views/home/Favorites.tsx
@@ -10,7 +10,7 @@ const Favorites = () => {
 			<Navbar>
 				<HomeToolbar />
 			</Navbar>
-			<SongsContainer title="Ulubione" songs={[]} />
+			{/* <SongsContainer title="Ulubione" songs={[]} /> */}
 			<Player />
 		</div>
 	);
