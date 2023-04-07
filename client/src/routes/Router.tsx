825410aff67420c1f910e938d2eb7ee73bc39958
Wronqa <kuba.wrona@onet.pl>
2023-04-07 22:16:11 +0200

    Created separate router component for prevent flashing when route

---
+++ client/src/routes/Router.tsx
@@ -0,0 +1,55 @@
+import React, { useContext } from 'react';
+import {
+	createBrowserRouter,
+	Navigate,
+	RouterProvider,
+} from 'react-router-dom';
+import ForgotPassword from '../views/auth/ForgotPassword';
+import SignIn from '../views/auth/SignIn';
+import SignUp from '../views/auth/SignUp';
+import GuestRoute from './GuestRoute';
+import Home from '../views/home/Home';
+import AuthRoute from './AuthRoute';
+import { AuthContext } from '../context/auth.context';
+
+const Router = () => {
+	const { state } = useContext(AuthContext);
+
+	if (state.user === null) return <></>;
+	const router = createBrowserRouter([
+		{
+			path: '/auth',
+			element: <GuestRoute />,
+			children: [
+				{
+					path: '',
+					element: <Navigate to="/auth/signin" replace />,
+				},
+				{
+					path: 'signin',
+					element: <SignIn />,
+				},
+				{ path: 'signup', element: <SignUp /> },
+				{ path: 'forgot', element: <ForgotPassword /> },
+			],
+		},
+		{
+			path: '/',
+			element: <AuthRoute />,
+			children: [
+				{
+					path: '/',
+					element: <Navigate to="/home" replace />,
+				},
+				{
+					path: '/home',
+					element: <Home />,
+				},
+			],
+		},
+	]);
+
+	return <RouterProvider router={router} />;
+};
+
+export default Router;
