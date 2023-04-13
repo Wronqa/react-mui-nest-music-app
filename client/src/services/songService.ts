4782b5ecfdb3c4ad6bc0816ad21ab60253cf509b
Wronqa <kuba.wrona@onet.pl>
2023-04-13 23:11:23 +0200

    New services

--- client/src/services/songService.ts
+++ client/src/services/songService.ts
@@ -1,5 +1,12 @@
+import { SongInterface } from '../shared/interfaces/song.interface';
 import { Api } from '../tools/Api';
 
 export const searchSongService = (query: string) => {
 	return Api.searchSong(query);
+};
+export const addSongToLibrary = (data: SongInterface) => {
+	return Api.addSongToLibrary(data);
+};
+export const deleteSongFromLibrary = (id: string) => {
+	return Api.deleteSongFromLibrary(id);
 };
