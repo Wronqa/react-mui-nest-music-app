b58086d6d82a078df64d29acce1518fef51b36a8
Wronqa <kuba.wrona@onet.pl>
2023-04-13 23:12:34 +0200

    Added functions to add song to library and remove from them

--- client/src/components/card/CustomCard.tsx
+++ client/src/components/card/CustomCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import {
 	Box,
@@ -12,12 +12,77 @@ import { link } from 'fs';
 import CardHeader from './CardHeader';
 import CardControl from './CardControl';
 import CardImage from './CardImage';
-import { SongInterface } from '../../shared/interfaces/song.interface';
+import {
+	CardControls,
+	CardFunctions,
+	SongInterface,
+	TYPES,
+} from '../../shared/interfaces/song.interface';
+import { filterControls } from '../../features/songs/tools/cardControlFilter';
+import { useMutation } from 'react-query';
+import {
+	addSongToLibrary,
+	deleteSongFromLibrary,
+} from '../../services/songService';
+import { AxiosError, AxiosResponse } from 'axios';
+import { statusNotifier } from '../../tools/statusNotifier';
+import { useNavigate } from 'react-router-dom';
 
 interface CustomCardProps {
 	song: SongInterface;
+	type: TYPES;
 }
-const CustomCard = ({ song }: CustomCardProps) => {
+const CustomCard = ({ song, type }: CustomCardProps) => {
+	const { mutateAsync: addSongMutation } = useMutation(addSongToLibrary);
+	const navigate = useNavigate();
+
+	const favoriteHandler = () => {
+		console.log('test');
+	};
+	const addHandler = async () => {
+		const addPromise = addSongMutation(song);
+		const toastId = 'add';
+
+		await statusNotifier<AxiosResponse>(addPromise, {
+			pendingText: 'Trwa dodawanie...',
+			successText: 'Dodano utwór do biblioteki',
+			toastId,
+		})
+			.then((response: AxiosResponse) => {
+				console.log('Dodano');
+			})
+			.catch((err: AxiosError) => {
+				console.log(err);
+			});
+	};
+	const deleteHandler = async () => {
+		const deletePromise = deleteSongFromLibrary(song.id);
+		const toastId = 'delete';
+
+		await statusNotifier<AxiosResponse>(deletePromise, {
+			pendingText: 'Trwa usuwanie...',
+			successText: 'Usunięto utwór z biblioteki',
+			toastId,
+		})
+			.then((response: AxiosResponse) => {
+				navigate('/');
+			})
+			.catch((err: AxiosError) => {
+				console.log(err);
+			});
+	};
+	const playHandler = () => {
+		console.log('play');
+	};
+
+	const filteredControls: CardControls = filterControls(type);
+	const cardFunctions: CardFunctions = {
+		add: addHandler,
+		remove: deleteHandler,
+		favorite: favoriteHandler,
+		play: playHandler,
+	};
+
 	return (
 		<Card
 			sx={{
@@ -38,7 +103,10 @@ const CustomCard = ({ song }: CustomCardProps) => {
 				}}
 			>
 				<CardHeader title={song.title} artist={song.artist} />
-				<CardControl />
+				<CardControl
+					controls={filteredControls}
+					controlFunctions={cardFunctions}
+				/>
 			</Box>
 			<CardImage picture={song.picture} />
 		</Card>
