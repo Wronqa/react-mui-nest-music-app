a8c1021462916e8c591ab4a9b251fee14a85ab60
Wronqa <kuba.wrona@onet.pl>
2023-04-13 21:26:20 +0200

    Created interface

--- client/src/components/card/CardControl.tsx
+++ client/src/components/card/CardControl.tsx
@@ -5,14 +5,28 @@ import SkipNextIcon from '@mui/icons-material/SkipNext';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 import FavoriteIcon from '@mui/icons-material/Favorite';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 import React from 'react';
 
-const CardControl = () => {
+interface CardControlProps {
+	play?: boolean;
+	add?: boolean;
+	favorite?: boolean;
+	remove?: boolean;
+}
+const CardControl = ({
+	play = true,
+	add,
+	favorite,
+	remove,
+}: CardControlProps) => {
 	return (
 		<Box sx={{ display: 'flex', alignItems: 'center', pl: 0.1, pb: 0.1 }}>
-			<IconButton aria-label="previous">
-				<PlayArrowIcon sx={{ height: 30, width: 30, color: 'green' }} />
-			</IconButton>
+			{play && (
+				<IconButton aria-label="previous">
+					<PlayArrowIcon sx={{ height: 30, width: 30, color: 'green' }} />
+				</IconButton>
+			)}
 			<IconButton aria-label="play/pause">
 				{Math.floor(Math.random() * 2) + 1 === 1 ? (
 					<FavoriteBorderIcon sx={{ height: 30, width: 30, color: 'red' }} />
@@ -23,6 +37,13 @@ const CardControl = () => {
 			<IconButton aria-label="next">
 				<DeleteForeverIcon sx={{ height: 30, width: 30, color: 'black' }} />
 			</IconButton>
+			{add && (
+				<IconButton aria-label="next">
+					<AddCircleOutlineIcon
+						sx={{ height: 30, width: 30, color: 'black' }}
+					/>
+				</IconButton>
+			)}
 		</Box>
 	);
 };
