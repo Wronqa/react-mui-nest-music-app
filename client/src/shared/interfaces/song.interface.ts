d00c1c1a597d36e97ed667ff3df9ef201d34d94d
Wronqa <kuba.wrona@onet.pl>
2023-04-13 23:11:15 +0200

    Added types for card controll

--- client/src/shared/interfaces/song.interface.ts
+++ client/src/shared/interfaces/song.interface.ts
@@ -1,11 +1,30 @@
 export interface SongsInterface {
 	songs: SongInterface[];
+	type: TYPES;
 }
 export interface SongInterface {
-	id: number;
+	id: string;
 	title: string;
 	duration: number;
 	preview: string;
 	artist: string;
 	picture: string;
+}
+////ZSoptymalizowac
+export interface CardControls {
+	play?: boolean;
+	add?: boolean;
+	favorite?: boolean;
+	remove?: boolean;
+}
+export interface CardFunctions {
+	play?: () => void;
+	add?: () => void;
+	favorite?: () => void;
+	remove?: () => void;
+}
+export enum TYPES {
+	'search',
+	'home',
+	'favorities',
 }
