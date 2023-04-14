4039bd33082f60a913a1b2fe137dfadc84887c14
Wronqa <kuba.wrona@onet.pl>
2023-04-14 23:29:26 +0200

    Deleted unnessesary imports

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import AuthContextProvider from './context/AuthContextProvider';
+import AuthContextProvider from './context/GlobalContextProvider';
 import {
 	useQuery,
 	useMutation,
