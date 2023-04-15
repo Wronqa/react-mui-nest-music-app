c9031f6298e395439d8012b3c5d406993de10925
Wronqa <kuba.wrona@onet.pl>
2023-04-15 23:59:12 +0200

    Updated interfaces names

--- client/src/context/reducers/player.reducer.ts
+++ client/src/context/reducers/player.reducer.ts
@@ -1,13 +1,10 @@
 import {
 	IPlayerAction,
-	PlayerInterface,
+	IPlayerState,
 } from '../../shared/interfaces/player.interface';
 import { playerActions } from '../actions/player.actions';
 
-export const playerReducer = (
-	state: PlayerInterface,
-	action: IPlayerAction
-) => {
+export const playerReducer = (state: IPlayerState, action: IPlayerAction) => {
 	const handler = playerActions[action.type];
 
 	return handler(state, action);
