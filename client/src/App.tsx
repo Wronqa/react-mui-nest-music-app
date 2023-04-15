64980b798dc9a08ecf9c4fdef2ecd930d8d2a329
Wronqa <kuba.wrona@onet.pl>
2023-04-15 23:51:32 +0200

    Updated interfaces names

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,23 +4,21 @@ import { useMutation } from 'react-query';
 import { checkAuthenticationService } from './services/authServices';
 import Router from './routes/Router';
 import { AxiosError, AxiosResponse } from 'axios';
-import { GlobalContext } from './context/GlobalContext';
-import { AUTH_ACTIONS } from './shared/interfaces/auth.interface';
+import { AuthActions } from './shared/interfaces/auth.interface';
+import AuthContext from './context/contexts/AuthContext';
 
 function App() {
-	const { authDispatch } = useContext(GlobalContext);
-	const { mutate, isSuccess } = useMutation({
+	const { dispatch: authDispatch } = useContext(AuthContext);
+	const { mutate } = useMutation({
 		mutationFn: checkAuthenticationService,
 		onSuccess(response: AxiosResponse) {
-			authDispatch({ type: AUTH_ACTIONS.loadUser, payload: response.data });
+			authDispatch({ type: AuthActions.LOAD_USER, payload: response.data });
 		},
 		onError(error: AxiosError) {
 			console.log(error);
-			authDispatch({ type: AUTH_ACTIONS.loadUser, payload: '' });
+			authDispatch({ type: AuthActions.LOAD_USER, payload: '' });
 		},
 	});
-
-	console.log('Re render');
 
 	useEffect(() => {
 		mutate();
