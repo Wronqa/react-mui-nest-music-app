0a82bdcbace0d53f1928e853114a94b322ce5b36
Wronqa <kuba.wrona@onet.pl>
2023-04-15 15:14:44 +0200

    Added player properties from context

--- client/src/features/songs/components/Player.tsx
+++ client/src/features/songs/components/Player.tsx
@@ -1,8 +1,13 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import AudioPlayer, { InterfacePlacement } from 'react-modern-audio-player';
+import PlayerContext from '../../../context/contexts/PlayerContext';
 
 const Player = () => {
 	const [autoplay, setAutoplay] = useState<boolean>(false);
+
+	const { state } = useContext(PlayerContext);
+
+	console.log(state);
 	const playList = [
 		{
 			name: 'Disney',
@@ -11,24 +16,12 @@ const Player = () => {
 			src: 'https://cdns-preview-e.dzcdn.net/stream/c-e674ea4ca1b24cbc6b65dc65934c1c21-4.mp3',
 			id: 1,
 		},
-		{
-			name: 'Eldorado',
-			writer: 'Sanah',
-			img: 'https://img.wprost.pl/img/sanah/4a/f7/fe7ed072a4126b5792e81d475b4a.jpeg',
-			src: 'https://cdns-preview-e.dzcdn.net/stream/c-e674ea4ca1b24cbc6b65dc65934c1c21-4.mp3',
-			id: 2,
-		},
 	];
 	return (
 		<div>
 			<AudioPlayer
-				playList={playList}
-				audioInitialState={{
-					isPlaying: autoplay,
-					muted: false,
-					volume: 0.2,
-					curPlayId: 1,
-				}}
+				playList={state.playlist}
+				audioInitialState={state.audioInitialState}
 				placement={{
 					player: 'bottom-left',
 				}}
