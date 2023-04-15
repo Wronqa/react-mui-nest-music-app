d900a267c263497e24425ff7f85cc46d05e8a924
Wronqa <kuba.wrona@onet.pl>
2023-04-15 23:59:22 +0200

    Updated interfaces names

--- client/src/context/reducers/songs.reducer.ts
+++ client/src/context/reducers/songs.reducer.ts
@@ -1,13 +1,10 @@
 import {
-	SongsActionInterface,
-	SongsStateInterface,
+	ISongsAction,
+	ISongsState,
 } from '../../shared/interfaces/song.interface';
 import { songsActions } from '../actions/songs.actions';
 
-export const songsReducer = (
-	state: SongsStateInterface,
-	action: SongsActionInterface
-) => {
+export const songsReducer = (state: ISongsState, action: ISongsAction) => {
 	const handler = songsActions[action.type];
 
 	return handler(state, action);
