76ff831669c7ffb679ce6ed19a42128dfaf3ae7d
Wronqa <kuba.wrona@onet.pl>
2023-04-15 15:15:49 +0200

    Created player context provider

---
+++ client/src/context/providers/PlayerContextProvider.tsx
@@ -0,0 +1,19 @@
+import React, { ReactNode, useMemo, useReducer } from 'react';
+import PlayerContext from '../contexts/PlayerContext';
+import { PLAYER_INITIAL_STATE } from '../initialStates/player.initialState';
+import { playerReducer } from '../reducers/player.reducer';
+
+interface IPlayerContextProviderProp {
+	children: ReactNode;
+}
+const PlayerContextProvider = ({ children }: IPlayerContextProviderProp) => {
+	const [state, dispatch] = useReducer(playerReducer, PLAYER_INITIAL_STATE);
+
+	const value = useMemo(() => ({ state, dispatch }), [state]);
+
+	return (
+		<PlayerContext.Provider value={value}>{children}</PlayerContext.Provider>
+	);
+};
+
+export default PlayerContextProvider;
