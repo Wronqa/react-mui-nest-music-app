25dc74f48787b5d0c90ad576953d109d4bd11f92
Wronqa <kuba.wrona@onet.pl>
2023-04-15 15:14:26 +0200

    New interface created

---
+++ client/src/shared/interfaces/player.interface.ts
@@ -0,0 +1,39 @@
+import { Dispatch } from 'react';
+import { AudioPlayerProps } from 'react-modern-audio-player';
+
+export interface PlayerContextInterface {
+	state: PlayerInterface;
+	dispatch: Dispatch<IPlayerAction>;
+}
+export interface PlayerInterface {
+	playlist: PlayerSongInterface[];
+	audioInitialState: AudioPlayerPropsInterface;
+}
+interface PlayerSongInterface {
+	name: string;
+	writer: string;
+	img: string;
+	src: string;
+	id: number;
+}
+interface AudioPlayerPropsInterface {
+	isPlaying: boolean;
+	muted: boolean;
+	volume: number;
+	curPlayId: number;
+}
+export interface IPlayerAction {
+	type: PlayerActions;
+	payload: IPlayerActionPayload;
+}
+export interface IPlayerActionPayload {
+	song: PlayerSongInterface;
+	options: IPlayerOptionsInterface;
+}
+interface IPlayerOptionsInterface {
+	curPlayId: number;
+	isPlaying: boolean;
+}
+export enum PlayerActions {
+	PLAY = 'play',
+}
