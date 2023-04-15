19d693b6872805bb00983b8caeac6a8b5a441dc6
Wronqa <kuba.wrona@onet.pl>
2023-04-15 23:55:36 +0200

    Created separate files for initial states

---
+++ client/src/context/initialStates/auth.initialState.ts
@@ -0,0 +1,5 @@
+import { IAuthState } from '../../shared/interfaces/auth.interface';
+
+export const AUTH_INITIAL_VALUE: IAuthState = {
+	user: null,
+};
---
+++ client/src/context/initialStates/songs.initialState.ts
@@ -0,0 +1,5 @@
+import { ISongsState } from '../../shared/interfaces/song.interface';
+
+export const SONGS_INITIAL_VALUE: ISongsState = {
+	songs: [],
+};
