dceb09cbd0679b9c300d0ce0b0777e3a481f5643
Wronqa <kuba.wrona@onet.pl>
2023-04-15 23:51:47 +0200

    Added nestes context providers

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,28 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import AuthContextProvider from './context/GlobalContextProvider';
-import {
-	useQuery,
-	useMutation,
-	useQueryClient,
-	QueryClient,
-	QueryClientProvider,
-} from 'react-query';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import PlayerContextProvider from './context/providers/PlayerContextProvider';
+import AuthContextProvider from './context/providers/AuthContextProvider';
+import SongsContextProvider from './context/providers/SongsContextProvider';
 
 const queryClient = new QueryClient();
+
+console.log('Test');
 
 const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement
 ).render(
 	<React.StrictMode>
 		<AuthContextProvider>
-			<PlayerContextProvider>
-				<QueryClientProvider client={queryClient}>
-					<App />
-				</QueryClientProvider>
-			</PlayerContextProvider>
+			<SongsContextProvider>
+				<PlayerContextProvider>
+					<QueryClientProvider client={queryClient}>
+						<App />
+					</QueryClientProvider>
+				</PlayerContextProvider>
+			</SongsContextProvider>
 		</AuthContextProvider>
 	</React.StrictMode>
 );
