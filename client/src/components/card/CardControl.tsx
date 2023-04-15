b4ec0ecbb7bc50dd1fa73b30eac717f3fbd172fd
Wronqa <kuba.wrona@onet.pl>
2023-04-15 16:07:40 +0200

    Added padding

--- client/src/components/card/CardControl.tsx
+++ client/src/components/card/CardControl.tsx
@@ -6,7 +6,9 @@ import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import PauseIcon from '@mui/icons-material/Pause';
 import React from 'react';
+import PlaylistAddIcon from '@mui/icons-material/PlaylistAdd';
 import {
 	CardControls,
 	CardFunctions,
@@ -16,37 +18,65 @@ interface CardControlProps {
 	controls: CardControls;
 	controlFunctions: CardFunctions;
 	isFavorite: boolean;
+	isPlaying: boolean;
 }
 const CardControl = ({
 	controls,
 	controlFunctions,
 	isFavorite,
+	isPlaying,
 }: CardControlProps) => {
 	return (
 		<Box sx={{ display: 'flex', alignItems: 'center', pl: 0.1, pb: 0.1 }}>
 			{controls.play && (
-				<IconButton aria-label="previous" onClick={controlFunctions.play}>
-					<PlayArrowIcon sx={{ height: 30, width: 30, color: 'green' }} />
+				<IconButton
+					aria-label="previous"
+					sx={{ pr: 0 }}
+					onClick={controlFunctions.play}
+				>
+					{isPlaying ? (
+						<PauseIcon sx={{ height: 27, width: 27, color: 'green' }} />
+					) : (
+						<PlayArrowIcon sx={{ height: 27, width: 27, color: 'green' }} />
+					)}
 				</IconButton>
 			)}
+			{controls.addToQueue && (
+				<IconButton
+					aria-label="next"
+					sx={{ pr: 0 }}
+					onClick={controlFunctions.addToQueue}
+				>
+					<PlaylistAddIcon sx={{ height: 27, width: 27, color: 'blue' }} />
+				</IconButton>
+			)}
+
 			{controls.favorite && (
-				<IconButton aria-label="play/pause" onClick={controlFunctions.like}>
+				<IconButton
+					aria-label="play/pause"
+					sx={{ pr: 0 }}
+					onClick={controlFunctions.like}
+				>
 					{!isFavorite ? (
-						<FavoriteBorderIcon sx={{ height: 30, width: 30, color: 'red' }} />
+						<FavoriteBorderIcon sx={{ height: 27, width: 27, color: 'red' }} />
 					) : (
-						<FavoriteIcon sx={{ height: 30, width: 30, color: 'red' }} />
+						<FavoriteIcon sx={{ height: 27, width: 27, color: 'red' }} />
 					)}
 				</IconButton>
 			)}
 			{controls.remove && (
 				<IconButton aria-label="next" onClick={controlFunctions.remove}>
-					<DeleteForeverIcon sx={{ height: 30, width: 30, color: 'black' }} />
+					<DeleteForeverIcon sx={{ height: 27, width: 27, color: 'black' }} />
 				</IconButton>
 			)}
 			{controls.add && (
-				<IconButton aria-label="next" onClick={controlFunctions.add}>
+				<IconButton
+					aria-label="next"
+					sx={{ pr: 0 }}
+					onClick={controlFunctions.add}
+				>
 					<AddCircleOutlineIcon
-						sx={{ height: 30, width: 30, color: 'black' }}
+						sx={{ height: 27, width: 27, color: 'black' }}
 					/>
 				</IconButton>
 			)}
