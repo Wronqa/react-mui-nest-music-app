bfd7c348cc7d7a7071bdefa754818c30b23311ad
Wronqa <kuba.wrona@onet.pl>
2023-04-15 18:22:07 +0200

    Modified props structure

--- client/src/components/card/CardControl.tsx
+++ client/src/components/card/CardControl.tsx
@@ -7,25 +7,55 @@ import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 import PauseIcon from '@mui/icons-material/Pause';
-import React from 'react';
+import React, { useContext } from 'react';
 import PlaylistAddIcon from '@mui/icons-material/PlaylistAdd';
+import PlaylistRemoveIcon from '@mui/icons-material/PlaylistRemove';
 import {
 	CardControls,
 	CardFunctions,
+	SongInterface,
 } from '../../shared/interfaces/song.interface';
+import PlayerContext from '../../context/contexts/PlayerContext';
 
+interface ISongStatus {
+	isFavorite: boolean;
+	isInQueue: boolean;
+}
 interface CardControlProps {
 	controls: CardControls;
 	controlFunctions: CardFunctions;
-	isFavorite: boolean;
-	isPlaying: boolean;
+	status: ISongStatus;
+	song: SongInterface;
 }
 const CardControl = ({
 	controls,
 	controlFunctions,
-	isFavorite,
-	isPlaying,
+	song,
+	status,
 }: CardControlProps) => {
+	const { dispatch: playerDispatch, state } = useContext(PlayerContext);
+
+	const isPlaying = () => {
+		if (
+			+state.audioInitialState.curPlayId === +song.id &&
+			state.audioInitialState.isPlaying
+		)
+			return true;
+		else return false;
+	};
+	const isInQueue = () => {
+		if (
+			state.playlist.filter((s) => +s.id === +song.id).length &&
+			state.playlist.length > 1
+		)
+			return true;
+		else return false;
+	};
+	const canAddToQueue = () => {
+		if (+state.audioInitialState.curPlayId !== +song.id) return true;
+		else return false;
+	};
+
 	return (
 		<Box sx={{ display: 'flex', alignItems: 'center', pl: 0.1, pb: 0.1 }}>
 			{controls.play && (
@@ -34,22 +64,38 @@ const CardControl = ({
 					sx={{ pr: 0 }}
 					onClick={controlFunctions.play}
 				>
-					{isPlaying ? (
+					{isPlaying() ? (
 						<PauseIcon sx={{ height: 27, width: 27, color: 'green' }} />
 					) : (
 						<PlayArrowIcon sx={{ height: 27, width: 27, color: 'green' }} />
 					)}
 				</IconButton>
 			)}
-			{controls.addToQueue && (
-				<IconButton
-					aria-label="next"
-					sx={{ pr: 0 }}
-					onClick={controlFunctions.addToQueue}
-				>
-					<PlaylistAddIcon sx={{ height: 27, width: 27, color: 'blue' }} />
-				</IconButton>
-			)}
+
+			{controls.addToQueue &&
+				(canAddToQueue() && !state.audioInitialState.isPlaying ? (
+					<IconButton
+						aria-label="next"
+						sx={{ pr: 0 }}
+						onClick={
+							!isInQueue()
+								? controlFunctions.addToQueue
+								: controlFunctions.deleteFromQueue
+						}
+					>
+						{!isInQueue() ? (
+							<PlaylistAddIcon sx={{ height: 27, width: 27, color: 'black' }} />
+						) : (
+							<PlaylistRemoveIcon
+								sx={{ height: 27, width: 27, color: 'black' }}
+							/>
+						)}
+					</IconButton>
+				) : (
+					<IconButton aria-label="next" sx={{ pr: 0 }}>
+						<PlaylistAddIcon sx={{ height: 27, width: 27, color: 'gray' }} />
+					</IconButton>
+				))}
 
 			{controls.favorite && (
 				<IconButton
@@ -57,7 +103,7 @@ const CardControl = ({
 					sx={{ pr: 0 }}
 					onClick={controlFunctions.like}
 				>
-					{!isFavorite ? (
+					{!status.isFavorite ? (
 						<FavoriteBorderIcon sx={{ height: 27, width: 27, color: 'red' }} />
 					) : (
 						<FavoriteIcon sx={{ height: 27, width: 27, color: 'red' }} />
