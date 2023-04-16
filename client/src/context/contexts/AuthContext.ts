98ec118900d1457344480af3b24f223b7787ee9e
Wronqa <kuba.wrona@onet.pl>
2023-04-16 20:32:10 +0200

    Added "type" in imports

--- client/src/context/contexts/AuthContext.ts
+++ client/src/context/contexts/AuthContext.ts
@@ -1,5 +1,5 @@
 import { createContext } from 'react';
-import { IAuthContext } from '../../shared/interfaces/auth.interface';
+import { type IAuthContext } from '../../shared/interfaces/auth.interface';
 import { AUTH_INITIAL_VALUE } from '../initialStates/auth.initialState';
 
 const AuthContext = createContext<IAuthContext>({
