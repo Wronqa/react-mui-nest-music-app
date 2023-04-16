aa1a0a0bf2256497a09d86421a5b5bdf3399d6a1
Wronqa <kuba.wrona@onet.pl>
2023-04-16 20:39:12 +0200

    Edited as to satisfied

--- client/src/features/authorization/components/ForgotPasswordForm.tsx
+++ client/src/features/authorization/components/ForgotPasswordForm.tsx
@@ -3,23 +3,25 @@ import React, { useState } from 'react';
 import { forgotPasswordSchema } from '../schemas/forgotPasswordSchema';
 import { useValidate } from '../hooks/useValidate';
 import Form from '../../../components/common/Form';
-import { ForgotPasswordInterface } from '../../../shared/types';
+import { type ForgotPasswordInterface } from '../../../shared/types';
 
 const ForgotPasswordForm = () => {
 	const [email, setEmail] = useState<string>('');
 
 	const errors = useValidate(
-		{ email } as ForgotPasswordInterface,
+		{ email } satisfies ForgotPasswordInterface,
 		forgotPasswordSchema
 	);
 
-	const onSend = async (e: React.MouseEvent<HTMLButtonElement>) => {
+	const onSend = (e: React.MouseEvent<HTMLButtonElement>) => {
 		e.preventDefault();
 	};
 
 	return (
 		<Form
-			handleClick={(e) => onSend(e)}
+			handleClick={(e) => {
+				onSend(e);
+			}}
 			text="Przypomnij hasło!"
 			options={{ signup: false, forgotPassword: false }}
 		>
@@ -33,10 +35,12 @@ const ForgotPasswordForm = () => {
 				autoComplete="email"
 				autoFocus
 				value={email}
-				onChange={(e) => setEmail(e.target.value)}
+				onChange={(e) => {
+					setEmail(e.target.value);
+				}}
 			/>
 			<Typography sx={{ color: 'red', fontSize: '0.8rem' }}>
-				{errors && errors[0]}
+				{errors?.[0]}
 			</Typography>
 		</Form>
 	);
