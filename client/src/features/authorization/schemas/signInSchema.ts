102758865fc53b2196b527fb73e1bcc0e4818437
Wronqa <kuba.wrona@onet.pl>
2023-04-16 20:40:54 +0200

    Edited function type

--- client/src/features/authorization/schemas/signInSchema.ts
+++ client/src/features/authorization/schemas/signInSchema.ts
@@ -1,13 +1,14 @@
-import { object, string, number, date, InferType } from 'yup';
+import { object, string } from 'yup';
 import { authMessages } from '../../../messages/localizedMessages';
 
-export let signInSchema = object({
+export const signInSchema = object({
 	email: string()
 		.email(authMessages.email_invalid)
 		.required(authMessages.required),
 	password: string()
 		.required(authMessages.required)
 		.matches(
+			// eslint-disable-next-line no-useless-escape
 			/^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?])[A-Za-z\d!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]{8,}$/,
 			authMessages.password_strong
 		),
