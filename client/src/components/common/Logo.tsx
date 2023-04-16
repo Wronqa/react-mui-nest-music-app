a215c2c888df243cef321812f20e95e634dd741a
Wronqa <kuba.wrona@onet.pl>
2023-04-16 20:29:39 +0200

    Deleted unnsessary imports

--- client/src/components/common/Logo.tsx
+++ client/src/components/common/Logo.tsx
@@ -1,4 +1,5 @@
-import { Box, Typography } from '@mui/material';
+import React from 'react';
+import { Typography } from '@mui/material';
 import MusicVideoIcon from '@mui/icons-material/MusicVideo';
 
 const Logo = () => {
