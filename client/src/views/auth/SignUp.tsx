51245e879a05da89aa87b7119aa5b5193df639b4
Wronqa <kuba.wrona@onet.pl>
2023-04-16 20:27:12 +0200

    Added "type" in imports

--- client/src/views/auth/SignUp.tsx
+++ client/src/views/auth/SignUp.tsx
@@ -1,9 +1,6 @@
-import { Box, Typography, Button, Container } from '@mui/material';
 import React from 'react';
-import Logo from '../../components/common/Logo';
+import { Container } from '@mui/material';
 import Navbar from '../../components/common/Navbar';
-import Form from '../../components/common/Form';
-import SignInForm from '../../features/authorization/components/SignInForm';
 import SignUpForm from '../../features/authorization/components/SignUpForm';
 import AuthToolbar from '../../features/navigation/AuthToolbar';
 
