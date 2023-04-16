d186d7d28c13ab1a3098f0e710584f94570e40e7
Wronqa <kuba.wrona@onet.pl>
2023-04-16 20:33:00 +0200

    Added "type" in imports

--- client/src/context/contexts/SongsContext.ts
+++ client/src/context/contexts/SongsContext.ts
@@ -1,5 +1,5 @@
 import { createContext } from 'react';
-import { ISongsContext } from '../../shared/interfaces/song.interface';
+import { type ISongsContext } from '../../shared/interfaces/song.interface';
 import { SONGS_INITIAL_VALUE } from '../initialStates/songs.initialState';
 
 export const SongsContext = createContext<ISongsContext>({
