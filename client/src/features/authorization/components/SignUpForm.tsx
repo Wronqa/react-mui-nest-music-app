9bb6c99ea36febde72c794acac9f35396f480616
Wronqa <kuba.wrona@onet.pl>
2023-04-17 21:39:51 +0200

    Edited imports

--- client/src/features/authorization/components/SignUpForm.tsx
+++ client/src/features/authorization/components/SignUpForm.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import Form from '../../../components/common/Form';
+import Form from '../../../components/common/form/Form';
 import { TextField, Typography } from '@mui/material';
 import { useValidate } from '../hooks/useValidate';
 import { type RegisterDataInterface } from '../../../shared/types';
