99f62c7864a2a7168d28bd976afe0ffbd0eae2a1
Wronqa <kuba.wrona@onet.pl>
2023-04-17 21:39:35 +0200

    Edited imports

--- client/src/views/admin/Manage.tsx
+++ client/src/views/admin/Manage.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import AdminToolbar from '../../features/navigation/AdminToolbar';
-import Navbar from '../../components/common/Navbar';
+import Navbar from '../../components/common/navbar/Navbar';
 
 const Manage = () => {
 	return (
